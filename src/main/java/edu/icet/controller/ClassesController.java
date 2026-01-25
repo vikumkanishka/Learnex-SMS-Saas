2c5af1a69841cbc58a63cabdd2f3823f10d15660
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-01-25 23:35:13 +0530

    Implement class management: add ClassDto, ClassEntity, ClassRepository, ClassService, and ClassServiceImpl with CRUD operations

--- src/main/java/edu/icet/controller/ClassesController.java
+++ src/main/java/edu/icet/controller/ClassesController.java
@@ -1,7 +1,46 @@
 package edu.icet.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import edu.icet.dto.ClassDto;
+import edu.icet.service.ClassService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class ClassesController {
+@RequestMapping(name = "classes")
+@RequiredArgsConstructor
+public class ClassesController implements ClassService{
+
+    final ClassService service;
+
+    @PostMapping("/add-class")
+    public void addClass(@RequestBody ClassDto classDto) {
+        service.addClass(classDto);
+    }
+
+    @PutMapping("/update-class")
+    public void updateClass(@RequestBody ClassDto classDto) {
+        service.updateClass(classDto);
+    }
+
+    @DeleteMapping("/delete-class/{id}")
+    public void deleteClass(@PathVariable Integer id) {
+        service.deleteClass(id);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public ClassDto searchById(@PathVariable Integer id) {
+        return service.searchById(id);
+    }
+
+    @GetMapping("/get-all-classes")
+    public List<ClassDto> getAllClasses() {
+        return service.getAllClasses();
+    }
+
+    @GetMapping("/search-by-name/{name}")
+    public List<ClassDto> searchClassByName(@PathVariable String name) {
+        return service.searchClassByName(name);
+    }
 }
