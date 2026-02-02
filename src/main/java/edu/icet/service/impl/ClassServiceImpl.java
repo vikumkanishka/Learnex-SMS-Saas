0d18cb68c83d56b4115c2044f7dc9f846c003c95
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-02 22:10:56 +0530

    Refactor ClassServiceImpl to use Spring annotations and dependency injection

--- src/main/java/edu/icet/service/impl/ClassServiceImpl.java
+++ src/main/java/edu/icet/service/impl/ClassServiceImpl.java
@@ -2,10 +2,17 @@ package edu.icet.service.impl;
 
 import edu.icet.dto.ClassDto;
 import edu.icet.service.ClassService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class ClassServiceImpl implements ClassService {
+
+    final ClassService service;
+
     @Override
     public void addClass(ClassDto classDto) {
 
